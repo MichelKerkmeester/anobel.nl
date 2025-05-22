8dac1bcb8cb1079b3a551c2c0659893636a07ee2
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2025-05-22 16:34:03 +0200

    cvb ncvzzncvfbxnvb

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,18 @@
 // This file imports all components and initializes them
 
 // Import components
-import "../C__sections/navigation/mega-menu.js";
-import "../C__sections/navigation/dropdown-menu.js";
-import "../C__sections/navigation/language-selector.js";
-import "../D__other/cursor/cursor.js";
-import "../B__components/button/btn--text-link.js";
-import "../B__components/button/btn--cta.js";
-import "../B__components/link/link--hero.js";
-import "../B__components/link/link--general.js";
-import "../C__sections/cms/pagination.js";
-import "../C__sections/hero/video/video.js";
-import "../D__other/legal/cookie-consent.js";
-import "../C__sections/grid/afdeling--call-to-action.js";
+import "../D__sections/navigation/mega-menu.js";
+import "../D__sections/navigation/dropdown-menu.js";
+import "../D__sections/navigation/language-selector.js";
+import "../E__other/cursor/cursor.js";
+import "../C__components/button/btn--text-link.js";
+import "../C__components/button/btn--cta.js";
+import "../C__components/link/link--hero.js";
+import "../C__components/link/link--general.js";
+import "../D__sections/cms/pagination.js";
+import "../D__sections/hero/video/video.js";
+import "../E__other/legal/cookie-consent.js";
+import "../D__sections/grid/afdeling--call-to-action.js";
 
 // Initialize any global functionality
 console.log("Nobel website scripts initialized");
