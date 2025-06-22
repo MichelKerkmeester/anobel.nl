4aab8b194ae6e4715511c7a5aa4dcbb4150fb3ee
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2025-06-22 10:12:50 +0200

    3.2

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,7 @@
 // This file imports all components and initializes them
 
 // Import components
-import "../code/C__components/navigation/mega-menu.js";
-import "../code/C__components/navigation/dropdown-menu.js";
-import "../code/C__components/navigation/language-selector.js";
-import "../code/C__components/button/btn--text-link.js";
-import "../code/C__components/button/btn--cta.js";
-import "../code/C__components/link/link--hero.js";
-import "../code/C__components/link/link--general.js";
-import "../code/C__components/cms/related-articles.js";
-import "../code/C__components/hero/hero--video.js";
-import "../code/D__other/legal/cookie-consent.js";
-import "../code/C__components/grid/afdeling--cta-hover.js";
-import "../code/D__sections/webshop/tab-autoplay.js";
+import "../code/folder/sub-folder/example.js";
 
 // Initialize any global functionality
 console.log("Nobel website scripts initialized");
